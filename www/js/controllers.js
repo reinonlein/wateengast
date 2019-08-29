d80741cd038c585e074a63c219688ec1836c0e05
Rein <reinonlein@gmail.com>
2019-08-29 21:25:06 +0200

    Added post view counts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -95,15 +95,15 @@ angular.module('starter')
             $scope.post_category = data.data.post.categories[0].title ? data.data.post.categories[0]
                 .title : 'no category';
             $scope.post_content = $sce.trustAsHtml(data.data.post.content);
-            $scope.post_date = data.data.post.date;
-            $scope.post_authorName = data.data.post.author.first_name + " " + data.data.post.author.last_name;
-              if($scope.post_authorName.trim() == '')
-                $scope.post_authorName = "No Name";
-              $scope.post_authorImage = 'http://ionicframework.com/img/docs/mcfly.jpg';
-              $scope.post_image = data.data.post.thumbnail_images.full.url;
-              $scope.post_commentCount = data.data.post.comment_count;
-              //$scope.post_views = data.data.post.custom_fields.post_views_count[0];
-              $scope.post_url = data.data.post.url;
+            //$scope.post_date = data.data.post.date;
+            //$scope.post_authorName = data.data.post.author.first_name + " " + data.data.post.author.last_name;
+            //  if($scope.post_authorName.trim() == '')
+            //    $scope.post_authorName = "No Name";
+            //$scope.post_authorImage = 'http://ionicframework.com/img/docs/mcfly.jpg';
+            $scope.post_image = data.data.post.thumbnail_images.full.url;
+            $scope.post_commentCount = data.data.post.comment_count;
+            $scope.post_views = data.data.post.custom_fields.views[0];
+            $scope.post_url = data.data.post.url;
         }, function(err){
 
         })
