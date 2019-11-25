b34dbbc939679e056bc60da8cb8213c4c6303f13
Rein <reinonlein@gmail.com>
2019-11-25 22:36:41 +0100

    onderzoekje wordcloud 700 posts

---
+++ websites/reinonlein/liefde.py
@@ -0,0 +1,24 @@
+
+croissants = 1
+
+while my_love_for_you = True:
+    return everything
+
+
+if you_are_here:
+    happy = True
+    me = complete
+    life.append(['huisje', 'boompje', 'beestje']
+else:
+    my_heart.split()
+    break
+
+
+def baby():
+    my_gender = 'male'
+    if your_gender != my_gender:
+        return bonte_avond, kindje
+    else:
+        return krat_bier, slap_geouwehoer
+
+
