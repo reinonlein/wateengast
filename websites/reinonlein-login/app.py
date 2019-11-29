fcb80a9c53df8cfd23f51ae398379e43a87cb35e
ReinOnlein <reinonlein@gmail.com>
2019-11-29 15:43:47 +0100

    login gefixt op site

--- websites/reinonlein-login/app.py
+++ websites/reinonlein-login/app.py
@@ -5,10 +5,81 @@ from reinonlein.models import User, Woorden
 from reinonlein.forms import LoginForm, RegistrationForm
 from werkzeug.security import generate_password_hash, check_password_hash
 
+###### ROUTES ######
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/werk')
+def werk():
+    return render_template('werk.html')
+
+@app.route('/over_mij')
+def over_mij():
+    return render_template('over_mij.html')
+
+@app.route('/database', methods=['GET', 'POST'])
+def database():
+
+
+    addform = AddDatabaseForm()
+    delform = DeleteDatabaseForm()
+
+    if addform.woord.data and addform.validate():
+
+        woord = addform.woord.data
+        
+        session['woord'] = addform.woord.data
+        flash(f"Wat vet, je hebt zojuist een {session['woord']} in mijn database gestopt!")
+        
+        nieuw_woord = Woorden(woord)
+        db.session.add(nieuw_woord)
+        db.session.commit()
+        
+        return redirect(url_for('database'))
+
+    if delform.deletewoord.data and delform.validate():
+
+        deletewoord = delform.deletewoord.data
+        
+        session['deletewoord'] = delform.deletewoord.data
+        flash(f"Hoppa, je hebt net een {session['deletewoord']} uit mijn database gehaald...")
+        
+        delete_woord = Woorden.query.filter_by(woord=deletewoord).first()
+        db.session.delete(delete_woord)
+        db.session.commit()
+        
+        return redirect(url_for('database'))
+
+    woorden = Woorden.query.all()
+    return render_template('database.html', addform=addform, delform=delform, woorden=woorden)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('oeps.html'), 404
+
+
+@app.route('/naamgast', methods=['GET', 'POST'])
+def naamgast():
+
+    form = InfoForm()
+
+    if form.validate_on_submit():
+
+        session['gast'] = form.gast.data
+        session['boekjes'] = form.boekjes.data
+        flash(f"Ah, je naam is: {session['gast']} gast")
+        flash(f"Toe maar, en je wilt maar liefst {session['boekjes']} boekjes kopen zeg! Wauw...")
+        return redirect(url_for('naamgast'))
+
+    return render_template('naamgast.html', form=form)
+
 
 
 @app.route('/welcome')
