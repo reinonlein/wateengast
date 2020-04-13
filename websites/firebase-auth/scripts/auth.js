a4d49d4fbde4b830ebb1a83501430bb978a712ef
Rein <reinonlein@gmail.com>
2020-04-13 23:51:47 +0200

    halfway on auth

--- websites/firebase-auth/scripts/auth.js
+++ websites/firebase-auth/scripts/auth.js
@@ -1,1 +1,56 @@
-// setup materialize components
+// get data
+db.collection('guides').get().then(snapshot => {
+    setupGuides(snapshot.docs);
+});
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log('user logged in: ', user);
+    } else {
+        console.log('user logged out');
+    }
+});
+
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut()
+});
+
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // login
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
