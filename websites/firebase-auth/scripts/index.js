e446b994b2b3a9810d03d89af0392e5da2e8a9fd
Rein <reinonlein@gmail.com>
2020-04-14 22:10:05 +0200

    update auth section

--- websites/firebase-auth/scripts/index.js
+++ websites/firebase-auth/scripts/index.js
@@ -1,22 +1,39 @@
 const guideList = document.querySelector('.guides');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+  if (user) {
+    // toggle UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+}
 
 // setup guides
 const setupGuides = (data) => {
 
-  let html = '';
-  data.forEach(doc => {
-    const guide = doc.data();
-    const li = `
-      <li>
-        <div class="collapsible-header grey lighten-4">${guide.title}</div>
-        <div class="collapsible-body white">${guide.content}</div>
-      </li>
-    `;
-    html += li
-  });
+  if(data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4">${guide.title}</div>
+          <div class="collapsible-body white">${guide.content}</div>
+        </li>
+      `;
+      html += li
+    });
 
-  guideList.innerHTML = html;
+    guideList.innerHTML = html;
 
+  } else {
+    guideList.innerHTML = '<h6 class="center-align">Please login to view the guides</h6>';
+  }
 }
 
 // setup materialize components
