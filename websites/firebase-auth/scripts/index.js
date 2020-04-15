1e982dacedd4ad848268db16d25c0766cdbbed00
Rein <reinonlein@gmail.com>
2020-04-15 12:55:52 +0200

    update firebase auth course

--- websites/firebase-auth/scripts/index.js
+++ websites/firebase-auth/scripts/index.js
@@ -1,13 +1,30 @@
 const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details')
 
 const setupUI = (user) => {
   if (user) {
+    // show account info
+    db.collection('users').doc(user.uid).get().then(doc => {
+
+      const html = `
+      <div>Logged in as ${user.email}</div>
+      <div>You signed up on ${user.metadata.creationTime}</div>
+      <div>And your last login was on ${user.metadata.lastSignInTime}</div>
+      <div>This is your one line bio: ${doc.data().bio}</div>
+      `;
+      accountDetails.innerHTML = html;
+
+    })
+    
+
     // toggle UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
   } else {
+    // hide account info
+    accountDetails.innerHTML = '';
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
   }
