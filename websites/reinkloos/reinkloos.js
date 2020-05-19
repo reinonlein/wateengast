e2ca14908e7cf8a95810a9f27047ce45ab427a02
Rein <reinonlein@gmail.com>
2020-05-19 19:41:13 +0200

    create reinsite in bootstrap

--- websites/reinkloos/reinkloos.js
+++ websites/reinkloos/reinkloos.js
@@ -1,12 +1,28 @@
 const voornaam = document.querySelector("#voornaam");
 const achternaam = document.querySelector("#achternaam");
+const email = document.querySelector("#email");
 
 
 function switchVoornaam() {switch (voornaam.innerText) {
     case "REIN":
-        voornaam.innerText = "GAST";
+        voornaam.innerText = "DATA";
       break;
-    case "GAST":
+    case "DATA":
+        voornaam.innerText = "CREATIVE";
+      break;
+    case "CREATIVE":
+        voornaam.innerText = "WEB";
+      break;
+    case "WEB":
+      voornaam.innerText = "DEEP";
+    break;
+    case "DEEP":
+        voornaam.innerText = "BEAUTIFUL";
+      break;
+    case "BEAUTIFUL":
+        voornaam.innerText = "CONTACT";
+      break;
+    case "CONTACT":
         voornaam.innerText = "REIN";
       break;
   }
@@ -14,13 +30,25 @@ function switchVoornaam() {switch (voornaam.innerText) {
 
 function switchAchternaam() {switch (achternaam.innerText) {
     case "KLOOS":
-        achternaam.innerText = "ONLEIN";
+        achternaam.innerText = "SCIENCE";
       break;
-    case "ONLEIN":
-        achternaam.innerText = "GAST";
+    case "SCIENCE":
+        achternaam.innerText = "ANALYTICS";
       break;
-    case "GAST":
+    case "ANALYTICS":
+        achternaam.innerText = "VISUALISATION";
+      break;
+    case "VISUALISATION":
+        achternaam.innerText = "WRITING";
+      break;
+    case "WRITING":
+        achternaam.innerText = "LEARNING";
+      break;
+    case "LEARNING":
+        achternaam.innerText = "ME";
+    break;
+    case "ME":
         achternaam.innerText = "KLOOS";
-      break;
+    break;
   }
 };
