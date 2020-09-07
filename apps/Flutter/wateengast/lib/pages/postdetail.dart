62180fa226a9f32aed09535e2c01b1e405c7b3e9
Rein <reinonlein@gmail.com>
2020-09-07 10:31:22 +0200

    wateengast-app bijgewerkt

---
+++ apps/Flutter/wateengast/lib/pages/postdetail.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+
+class PostDetail extends StatefulWidget {
+  @override
+  _PostDetailState createState() => _PostDetailState();
+}
+
+class _PostDetailState extends State<PostDetail> {
+  Map post = {};
+
+  @override
+  Widget build(BuildContext context) {
+    post = ModalRoute.of(context).settings.arguments;
+
+    print(post);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wat een gast...'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 220,
+            child: FadeInImage.assetNetwork(
+              placeholder: 'images/loading.gif',
+              fit: BoxFit.cover,
+              image: post['image'],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 23.0, 15.0, 5.0),
+            child: Text(post['title'],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green[600],
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 15.0),
+            child: Html(
+              defaultTextStyle: TextStyle(fontSize: 15),
+              data: post['content'],
+              padding: EdgeInsets.all(10.0),
+              onLinkTap: (url) {
+                String site = url.substring(0, 26);
+                print("Opening $url...");
+                print("site is $site");
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Terug naar alle posts'),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    );
+  }
+}
