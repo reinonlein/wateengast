ff3efb390f94c1285aa50a50684e476aac40a1ac
Rein <reinonlein@gmail.com>
2020-09-07 12:22:46 +0200

    complete awesomeness toegevoegd

--- apps/Flutter/wateengast/lib/pages/home.dart
+++ apps/Flutter/wateengast/lib/pages/home.dart
@@ -20,6 +20,7 @@ class _HomeState extends State<Home> {
   ScrollController _scrollController = new ScrollController();
   bool loading = false;
   int page = 1;
+  List currentPostList = [];
 
   Future<List<SinglePost>> _getPosts() async {
     setState(() {
@@ -44,19 +45,23 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    futurePostList = _getPosts();
-    loading = false;
+    //futurePostList = _getPosts();
+    _getPosts().then((response) {
+      currentPostList = response;
+      setState(() {});
+    });
+    //loading = false;
 
-    // _scrollController.addListener(() {
-    //   if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-    //     // if we are at the bottom of the page
-    //     print('Tijd voor nieuwe posts!');
-    //     setState(() {
-    //       page += 1;
-    //       futurePostList = _getPosts();
-    //     });
-    //   }
-    // });
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        // if we are at the bottom of the page
+        page += 1;
+        _getPosts().then((response) {
+          currentPostList.addAll(response);
+          setState(() {});
+        });
+      }
+    });
   }
 
   _scrollToTop() {
@@ -78,105 +83,180 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: Center(
-        child: FutureBuilder<List>(
-          future: futurePostList,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.separated(
-                controller: _scrollController,
-                itemCount: snapshot.data.length + 1,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  if (index == snapshot.data.length) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            icon: Icon(Icons.arrow_back),
-                            onPressed: () {
-                              setState(() {
-                                if (page > 1) {
-                                  page -= 1;
-                                  futurePostList = _getPosts();
-                                  _getPosts().then((_) {
-                                    _scrollToTop();
-                                  });
-                                }
-                              });
-                              print(page);
-                            }),
-                        loading
-                            ? CircularProgressIndicator(
-                                strokeWidth: 2.0,
-                              )
-                            : SizedBox(width: 35.0),
-                        IconButton(
-                            icon: Icon(Icons.arrow_forward),
-                            onPressed: () {
-                              setState(() {
-                                page += 1;
-                                futurePostList = _getPosts();
-                                _getPosts().then((_) {
-                                  _scrollToTop();
-                                });
-                              });
-                              print(page);
-                            }),
-                      ],
-                    );
-                  } else {
-                    return new ListTile(
-                        title: Text(snapshot.data[index].title),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(6.0),
-                          child: FadeInImage.assetNetwork(
-                            placeholder: 'images/loadingbox.gif',
-                            image: snapshot.data[index].thumbnail,
-                          ),
+          child: ListView.separated(
+              controller: _scrollController,
+              itemCount: currentPostList.length + 1,
+              separatorBuilder: (BuildContext context, int index) => Divider(),
+              padding: EdgeInsets.all(10),
+              itemBuilder: (context, index) {
+                if (currentPostList.length == 0) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 200),
+                      Text('Welkom bij',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: CircularProgressIndicator(),
+                      ),
+                      Text('Wat een gast 2.0!',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                    ],
+                  );
+                  ;
+                } else if (index == currentPostList.length) {
+                  return new ListTile(
+                    leading: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(),
+                    ),
+                    title: Text('Een momentje!'),
+                    subtitle: Text('Er worden meer posts opgehaald...'),
+                  );
+                } else {
+                  return new ListTile(
+                      title: Text(currentPostList[index].title),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(6.0),
+                        child: FadeInImage.assetNetwork(
+                          placeholder: 'images/loadingbox.gif',
+                          image: currentPostList[index].thumbnail,
                         ),
-                        contentPadding: EdgeInsets.fromLTRB(15, 4, 5, 7),
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            '/postdetail',
-                            arguments: {
-                              'title': snapshot.data[index].title,
-                              'image': snapshot.data[index].image,
-                              'content': snapshot.data[index].content,
-                            },
-                          );
-                        });
-                  }
-                },
-                separatorBuilder: (BuildContext context, int index) => Divider(),
-                padding: EdgeInsets.all(10),
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            // By default, show a loading spinner.
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Welkom bij',
-                    style: TextStyle(
-                      fontSize: 20,
-                    )),
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: CircularProgressIndicator(),
-                ),
-                Text(
-                  'Wat een gast!',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
-      ),
+                      ),
+                      contentPadding: EdgeInsets.fromLTRB(15, 4, 5, 7),
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          '/postdetail',
+                          arguments: {
+                            'title': currentPostList[index].title,
+                            'image': currentPostList[index].image,
+                            'content': currentPostList[index].content,
+                          },
+                        );
+                      });
+                }
+              })
+
+          // child: FutureBuilder<List>(
+          //   future: futurePostList,
+          //   builder: (context, snapshot) {
+          //     if (snapshot.hasData) {
+          //       return ListView.separated(
+          //         controller: _scrollController,
+          //         itemCount: snapshot.data.length + 1,
+          //         itemBuilder: (BuildContext ctxt, int index) {
+          //           if (index == snapshot.data.length) {
+          //             return Row(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: [
+          //                 IconButton(
+          //                     icon: Icon(Icons.arrow_back),
+          //                     onPressed: () {
+          //                       setState(() {
+          //                         if (page > 1) {
+          //                           page -= 1;
+          //                           futurePostList = _getPosts();
+          //                           _getPosts().then((_) {
+          //                             _scrollToTop();
+          //                           });
+          //                         }
+          //                       });
+          //                       print(page);
+          //                     }),
+          //                 loading
+          //                     ? CircularProgressIndicator(
+          //                         strokeWidth: 2.0,
+          //                       )
+          //                     : SizedBox(width: 35.0),
+          //                 IconButton(
+          //                     icon: Icon(Icons.arrow_forward),
+          //                     onPressed: () {
+          //                       setState(() {
+          //                         page += 1;
+          //                         futurePostList = _getPosts();
+          //                         _getPosts().then((response) {
+          //                           currentPostList.addAll(response);
+          //                           print(currentPostList.length);
+          //                           _scrollToTop();
+          //                         });
+          //                       });
+          //                       print(page);
+          //                     }),
+          //               ],
+          //             );
+          //           } else {
+          //             return new ListTile(
+          //                 title: Text(snapshot.data[index].title),
+          //                 leading: ClipRRect(
+          //                   borderRadius: BorderRadius.circular(6.0),
+          //                   child: FadeInImage.assetNetwork(
+          //                     placeholder: 'images/loadingbox.gif',
+          //                     image: snapshot.data[index].thumbnail,
+          //                   ),
+          //                 ),
+          //                 contentPadding: EdgeInsets.fromLTRB(15, 4, 5, 7),
+          //                 onTap: () {
+          //                   Navigator.pushNamed(
+          //                     context,
+          //                     '/postdetail',
+          //                     arguments: {
+          //                       'title': snapshot.data[index].title,
+          //                       'image': snapshot.data[index].image,
+          //                       'content': snapshot.data[index].content,
+          //                     },
+          //                   );
+          //                 });
+          //           }
+          //         },
+          //         separatorBuilder: (BuildContext context, int index) => Divider(),
+          //         padding: EdgeInsets.all(10),
+          //       );
+          //     } else if (snapshot.hasError) {
+          //       return Text("${snapshot.error}");
+          //     }
+          //     // By default, show a loading spinner.
+          //     return Column(
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: [
+          //         Text('Welkom bij',
+          //             style: TextStyle(
+          //               fontSize: 20,
+          //             )),
+          //         Padding(
+          //           padding: const EdgeInsets.all(15),
+          //           child: CircularProgressIndicator(),
+          //         ),
+          //         Text(
+          //           'Wat een gast!',
+          //           style: TextStyle(
+          //             fontSize: 20,
+          //             color: Colors.black,
+          //           ),
+          //         ),
+          //       ],
+          //     );
+          //   },
+          // ),
+          ),
     );
   }
 }
+
+/* SNIPPETS - controller voor in initState(): 
+ _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        // if we are at the bottom of the page
+        print('Tijd voor nieuwe posts!');
+        setState(() {
+          page += 1;
+          futurePostList = _getPosts();
+        });
+      }
+    });
+*/
