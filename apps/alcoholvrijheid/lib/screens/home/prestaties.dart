a97aaa08ffb16d0a735fa8046b2dafd46cb5001e
Rein <reinonlein@gmail.com>
2020-11-02 12:24:48 +0100

    final push before release 1.3.0

--- apps/alcoholvrijheid/lib/screens/home/prestaties.dart
+++ apps/alcoholvrijheid/lib/screens/home/prestaties.dart
@@ -23,9 +23,9 @@ class Prestaties extends StatelessWidget {
           UserData userData = snapshot.data;
           int stopuren = Jiffy().diff(userData.stopdate, Units.HOUR);
           double stopmaanden = Jiffy().diff(userData.stopdate, Units.MONTH, true);
-          double liters = ((stopuren / (7 * 24)) * userData.bier * 0.3).round() +
+          int liters = ((stopuren / (7 * 24)) * userData.bier * 0.3).round() +
               ((stopuren / (7 * 24)) * userData.wijn * 0.125).round() +
-              ((stopuren / (7 * 24)) * userData.sterk * 0.035);
+              ((stopuren / (7 * 24)) * userData.sterk * 0.035).round();
           int katers = (stopmaanden * userData.katers).floor();
           return Scaffold(
             body: ListView.separated(
@@ -40,7 +40,7 @@ class Prestaties extends StatelessWidget {
                         color: stopuren > prestaties[index]['target']
                             ? Colors.amber
                             : Colors.grey[300],
-                        size: 42,
+                        size: 44,
                       ),
                       title: Text(prestaties[index]['title']),
                       subtitle: stopuren > prestaties[index]['target']
@@ -101,7 +101,7 @@ class Prestaties extends StatelessWidget {
                         color: stopmaanden > prestaties[index]['target']
                             ? Colors.amber
                             : Colors.grey[300],
-                        size: 42,
+                        size: 44,
                       ),
                       title: Text(prestaties[index]['title']),
                       subtitle: stopmaanden > prestaties[index]['target']
@@ -161,12 +161,12 @@ class Prestaties extends StatelessWidget {
                         Icons.emoji_events,
                         color:
                             liters > prestaties[index]['target'] ? Colors.amber : Colors.grey[300],
-                        size: 42,
+                        size: 44,
                       ),
                       title: Text(prestaties[index]['title']),
                       subtitle: liters > prestaties[index]['target']
                           ? Text(
-                              'Gehaald op ${Jiffy(Jiffy(userData.stopdate).add(months: prestaties[index]['target'])).yMMMMd}',
+                              'Gehaald op ${Jiffy(Jiffy(userData.stopdate).add(hours: (prestaties[index]['target'] / ((userData.bier * 0.3).round() + (userData.wijn * 0.125).round() + (userData.sterk * 0.035).round()).round() * 7 * 24).round())).yMMMMd}',
                               style: TextStyle(color: Colors.grey),
                             )
                           : LinearProgressIndicator(
@@ -221,12 +221,12 @@ class Prestaties extends StatelessWidget {
                         Icons.emoji_events,
                         color:
                             katers > prestaties[index]['target'] ? Colors.amber : Colors.grey[300],
-                        size: 42,
+                        size: 44,
                       ),
                       title: Text(prestaties[index]['title']),
                       subtitle: katers > prestaties[index]['target']
                           ? Text(
-                              'Gehaald op ${Jiffy(Jiffy(userData.stopdate).add(months: stopmaanden.floor())).yMMMMd}',
+                              'Gehaald op ${Jiffy(Jiffy(userData.stopdate).add(days: (prestaties[index]['target'] / (userData.katers / 30.5)).floor())).yMMMMd}',
                               style: TextStyle(color: Colors.grey),
                             )
                           : LinearProgressIndicator(
@@ -235,6 +235,7 @@ class Prestaties extends StatelessWidget {
                               backgroundColor: Colors.grey[300],
                             ),
                       onTap: () {
+                        print(stopmaanden * 30.5);
                         bool gehaald = katers > prestaties[index]['target'];
                         AwesomeDialog(
                           context: context,
@@ -283,12 +284,12 @@ class Prestaties extends StatelessWidget {
                         color: stopuren * (userData.geld / (7 * 24)) > prestaties[index]['target']
                             ? Colors.amber
                             : Colors.grey[300],
-                        size: 42,
+                        size: 44,
                       ),
                       title: Text(prestaties[index]['title']),
                       subtitle: stopuren * (userData.geld / (7 * 24)) > prestaties[index]['target']
                           ? Text(
-                              'Gehaald op ${Jiffy(Jiffy(userData.stopdate).add(months: prestaties[index]['target'])).yMMMMd}',
+                              'Gehaald op ${Jiffy(Jiffy(userData.stopdate).add(hours: (prestaties[index]['target'] / (userData.geld / (7 * 24))).round())).yMMMMd}',
                               style: TextStyle(color: Colors.grey),
                             )
                           : LinearProgressIndicator(
@@ -329,7 +330,7 @@ class Prestaties extends StatelessWidget {
                           title: gehaald ? prestaties[index]['title'] : 'Nog even geduld...',
                           desc: gehaald
                               ? prestaties[index]['content']
-                              : 'Je moet nog € ${((prestaties[index]['target'] - (stopuren * (userData.geld / (7 * 24))))).toStringAsFixed(2)} besparen om deze prestatie vrij te spelen.',
+                              : 'Je moet nog € ${((prestaties[index]['target'] - (stopuren * (userData.geld / (7 * 24))))).toStringAsFixed(2)} besparen om deze prestatie vrij te spelen.\n\nAls je zo doorgaat is dat over ${((((prestaties[index]['target'] - (stopuren * (userData.geld / (7 * 24)))) / (userData.geld / (7 * 24))).round()) / 24).round()} dagen op ${Jiffy(Jiffy(userData.stopdate).add(hours: (((prestaties[index]['target'] - (stopuren * (userData.geld / (7 * 24)))) / (userData.geld / (7 * 24))).round()))).yMMMMd}.',
                           padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 5.0),
                           btnOkText: gehaald ? 'Wat een feest!' : 'Ah balen...',
                           btnOkIcon: gehaald
