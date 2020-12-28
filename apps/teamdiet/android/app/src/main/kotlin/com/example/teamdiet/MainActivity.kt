c88c55adc192c6f0486c86dedb83d38f6dbfc70a
Rein <reinonlein@gmail.com>
2020-12-28 22:00:44 +0100

    sync stuff with lame commit comment

---
+++ apps/teamdiet/android/app/src/main/kotlin/com/example/teamdiet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teamdiet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps/teamdiet/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:teamdiet/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Team Diet',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
